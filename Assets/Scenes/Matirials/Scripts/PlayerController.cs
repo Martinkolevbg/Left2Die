ebcd0205b7cadf609d51a349343bf0cc37943903
Martin Kolev <martinkolevbg93@gmail.com>
2018-06-10 21:12:03 +0300

    Added player movment.

---
+++ Assets/Scenes/Matirials/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (CharacterController))]
+public class PlayerController : MonoBehaviour {
+    //Components
+    private CharacterController controller;
+    //Handling
+    public float rotationSpeed = 450;
+    public float runSpeed = 8 ;
+    public float walkSpeed = 5 ;
+
+    //System
+    private Quaternion targetRotation;
+
+    void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	void Update () {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        if (input != Vector3.zero)
+        {
+            targetRotation = Quaternion.LookRotation(input);
+            transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
+        }
+
+        Vector3 motion = input;
+        motion += Vector3.up * -8;
+
+        controller.Move(motion * Time.deltaTime * walkSpeed);
+
+	}
+}
